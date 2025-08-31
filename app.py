d82e3f3a8ad9de662b74c67f93e8242aa60cd9d7
sai-dabbiru <149162359+sai-dabbiru@users.noreply.github.com>
2025-08-31 18:38:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return "Hello from Flask running on GitHub Actions!"
 
+@app.route("/check")
+def check():
+    return "its working"
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
